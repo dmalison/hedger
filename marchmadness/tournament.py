a894faecd53b7d26997ef0f1246b51d6ce1feed2
David Malison <dmalison@uber.com>
2021-04-19 21:15:31 -0700

    Add utils

--- marchmadness/tournament.py
+++ marchmadness/tournament.py
@@ -1,12 +1,16 @@
-from Typing import List
-
-from team import Team
+import itertools
 
 
 class Tournament:
-    def __init__(self, teams: List[Team]):
-        self._teams = teams
-        self._matches = self._make_matches()
+    def __init__(self, entries):
+        self._entries = entries
+        self._matches = None
 
+    @property
+    def matches(self):
+        if self._matches is None:
+            self._make_matches()
+        return self._matches
+    
     def _make_matches(self):
-        print('making matches')
+        pass
--- tests/test_tournament.py
+++ tests/test_tournament.py
@@ -1,15 +1,17 @@
-# import unittest
+import unittest
 
-# from match import Match
-# from entry import Entry
-# from tournament import Tournament
+import marchmadness.entry as entry
+import marchmadness.match as match
+import marchmadness.tournament as tournament
 
 
-# class TournamentTest(unittest.TestCase):
-#     def test_make_rounds_with_two_teams(self):
-#         gryffindor = Entry('Gryffindor')
-#         slytherin = Entry('Slytherin')
-#         teams = [gryffindor, slytherin]
-#         tournament = Tournament(teams)
+class TournamentTest(unittest.TestCase):
+    def test_make_matches_with_two_teams(self):
+        gryffindor = entry.Entry('Gryffindor')
+        slytherin = entry.Entry('Slytherin')
+        entries = [gryffindor, slytherin]
+        quidditch_cup = tournament.Tournament(entries)
 
-#         # tournament._matches == [Match(Entry(gryffindor), Entry(slytherin))]
+        actual = [repr(match) for match in quidditch_cup.matches]
+        expected = ["Match('final').set_top(Entry('gryffindor')).set_bottom(Entry('slytherin'))"]
+        self.assertEqual(actual, expected)
